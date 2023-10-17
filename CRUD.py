72c75ae2eb333a58322ce1ad90a68b0ab9999e76
matheus de oliveira <nubinhopro5@gmail.com>
2023-10-16 23:02:52 -0300

    versões iniciais das funções, é necessário o tratamento de erros

--- CRUD.py
+++ CRUD.py
@@ -2,6 +2,7 @@ import pandas as pd
 def listamento():
     print('\n')
     print(df)
+    input('Aperte para sair...') # serve como um "aguarde"
 def cadastramento(): 
     nome = nascimento = ''
     cpf = altura = peso = 0
@@ -12,9 +13,37 @@ def cadastramento():
     peso = float(input('peso: '))
     df.loc[len(df)] = [nome, cpf, nascimento, altura, peso] # caso localize uma linha que não existe, ele cria uma nova
     df.to_json('Bd.json', force_ascii=False, orient='records') # salvo logo em seguida
+    input('Aperte para sair...')
+def consulta():
+    nome = input('Digite o nome exato a ser pesquisado: ')
+    if nome in df['nome'].values:
+        print(df[df['nome'] == nome]) # mostra o valor na linha do índice do nome digitado
+    input('Aperte para sair...')
+def edita():
+    nome = nascimento = ''
+    cpf = altura = peso = 0
+    indice = 0
+    nome = input('Digite o nome exato a ser editado: ')
+    if nome in df['nome'].values:
+        print(df[df['nome'] == nome]) # mostra o valor na linha do índice do nome digitado
+        indice = df.index[df['nome'] == nome].tolist() # guardo o índice da linha
+    nome = input('novo nome: ')
+    df.iloc[[indice], [0]] = nome # seleciono a linha que quero, e o valor a ser editado
+    cpf = int(input('novo CPF: '))
+    df.iloc[[indice], [1]] = cpf 
+    nascimento = input('nova data de nascimento: ')
+    df.iloc[[indice], [2]] = nascimento
+    altura = float(input('nova altura: '))
+    df.iloc[[indice], [3]] = altura
+    peso = float(input('novo peso: '))
+    df.iloc[[indice], [4]] = peso
+    df.to_json('Bd.json', force_ascii=False, orient='records') # salvo logo em seguida
+    input('Aperte para sair...')
 
 df = pd.read_json('Bd.json') # abre o arquivo
 while True:
+    print('--------------------------')
+    print(f'Pessoas cadastradas: {len(df)}')
     print('--------------------------')
     print('1 - listar')
     print('2 - cadastrar')
@@ -26,10 +55,12 @@ while True:
     print('--------------------------')
     escolha = int(input('Digite sua escolha: '))
     if escolha == 1:
-        listamento() # mostra todas as pessoas cadastradas
-        input('Aperte para sair...') # serve como um "aguarde" 
+        listamento() # mostra todas as pessoas cadastradas 
     elif escolha == 2:
         cadastramento() # cadastra uma pessoa nova
-        input('Aperte para sair...')
+    elif escolha == 3:
+        consulta() # consulta a pessoa no banco de dados
+    elif escolha == 4:
+        edita() # edita a pessoa escolhida
     elif escolha == 0:
         break
