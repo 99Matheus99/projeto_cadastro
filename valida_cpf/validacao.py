a4f15fb42b9d771c3daef42408171f62c7ca9d0c
matheus de oliveira <nubinhopro5@gmail.com>
2024-02-05 18:52:48 -0300

    parte da análise

--- valida_cpf/validacao.py
+++ valida_cpf/validacao.py
@@ -7,9 +7,28 @@ cpf = ''.join(filter(str.isdigit, cpf)) # tira o que não é dígito
 soma = digito1 = 0
 for n in range(0,9):
     soma += int(cpf[n]) * (10 - n)
-
 resto = soma % 11
 if(resto < 2):
     digito1 = 0
 else:
     digito1 = 11 - resto
+
+# SEGUNDO DIGITO VERIFICADOR
+soma = digito2 = 0
+for n in range(0,10):
+    soma += int(cpf[n]) * (11 - n)
+resto = soma % 11
+if(resto < 2):
+    digito2 = 0
+else:
+    digito2 = 11 - resto
+
+# PARTE DA ANÁLISE
+if digito1 == int(cpf[9]):
+    print('TRUE')
+else:
+    print('FALSE')
+if digito2 == int(cpf[10]):
+    print('TRUE')
+else:
+    print('FALSE')
